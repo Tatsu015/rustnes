a93fa49e92addee2b9f6af828abecc80f32e9f9b
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-03 22:34:28 +0900

    fix error

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,7 +1,7 @@
 pub struct CPU {
-    pub register_a: u8;
-    pub status: u8;
-    pub program_counter: u8;
+    pub register_a: u8,
+    pub status: u8,
+    pub program_counter: u8,
 }
 
 impl CPU {
@@ -16,7 +16,7 @@ impl CPU {
     pub fn interpret(&mut self, program: Vec<u8>){
         self.program_counter = 0;
 
-        loop{
+        loop {
             let opscode = program[self.program_counter as usize];
             self.program_counter +=1;
 
