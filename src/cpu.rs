0ad097cf3effc10d37339fb16c0909323c695954
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-03 22:38:24 +0900

    fmt

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,23 +5,23 @@ pub struct CPU {
 }
 
 impl CPU {
-    pub fn new()->Self{
-        CPU{
-            register_a:0,
-            status:0,
-            program_counter:0,
+    pub fn new() -> Self {
+        CPU {
+            register_a: 0,
+            status: 0,
+            program_counter: 0,
         }
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>){
+    pub fn interpret(&mut self, program: Vec<u8>) {
         self.program_counter = 0;
 
         loop {
             let opscode = program[self.program_counter as usize];
-            self.program_counter +=1;
+            self.program_counter += 1;
 
             match opscode {
-                _ => todo!("")
+                _ => todo!(""),
             }
         }
     }
