33310d34bc6be09516317101c68bd27edb727f30
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-22 08:32:02 +0900

    add bitflags

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+use bitflags::bitflags;
+
 #[derive(Debug)]
 #[allow(non_camel_case_types)]
 pub enum AddressingMode {
@@ -11,6 +13,19 @@ pub enum AddressingMode {
     Indirect_X,
     Indirect_Y,
     NoneAdressing,
+}
+
+bitflags! {
+    pub struct CpuFlags:u8 {
+        const CARRY = 0b0000_0001;
+        const ZERO = 0b0000_0010;
+        const INTERRUPT_DISABLE = 0b0000_0100;
+        const DECIMAL = 0b0000_1000;
+        const BREAK1=0b0001_0000;
+        const BREAK2=0b0010_0000;
+        const OVERFLOW = 0b0100_0000;
+        const NEGATIVE = 0b1000_0000;
+    }
 }
 
 pub struct CPU {
