65f71236747538dabdb36816d27043658efdc922
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-22 21:42:21 +0900

    rename

--- src/cpu.rs
+++ src/cpu.rs
@@ -21,8 +21,8 @@ bitflags! {
         const ZERO = 0b0000_0010;
         const INTERRUPT_DISABLE = 0b0000_0100;
         const DECIMAL = 0b0000_1000;
-        const BREAK1=0b0001_0000;
-        const BREAK2=0b0010_0000;
+        const BREAK=0b0001_0000;
+        const RESERVED=0b0010_0000;
         const OVERFLOW = 0b0100_0000;
         const NEGATIVE = 0b1000_0000;
     }
@@ -32,7 +32,7 @@ pub struct CPU {
     pub register_a: u8,
     pub register_x: u8,
     pub register_y: u8,
-    pub status: u8,
+    pub status: CpuFlags,
     pub program_counter: u16,
     pub memory: [u8; 0xFFFF],
 }
