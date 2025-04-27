3479db070bed905c1a7a2a185b27b7de75a91003
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-27 21:55:25 +0900

    add dec

--- src/cpu.rs
+++ src/cpu.rs
@@ -328,6 +328,15 @@ impl CPU {
         self.update_zero_and_negative_flags(self.register_y.wrapping_sub(data));
     }
 
+    #[allow(dead_code)]
+    fn dec(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_adress(mode);
+        let data = self.mem_read(addr);
+        let new_data = data.wrapping_sub(1);
+        self.mem_write(addr, new_data);
+        self.update_zero_and_negative_flags(new_data);
+    }
+
     fn branch(&mut self, condition: bool) {
         if condition {
             let jump = self.mem_read(self.program_counter);
