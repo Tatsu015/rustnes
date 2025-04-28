47fb4095ed2ee369b54303bebf58bcf5246071c6
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-28 22:10:19 +0900

    add dey

--- src/cpu.rs
+++ src/cpu.rs
@@ -343,6 +343,12 @@ impl CPU {
         self.update_zero_and_negative_flags(self.register_x);
     }
 
+    #[allow(dead_code)]
+    fn dey(&mut self) {
+        self.register_y = self.register_y.wrapping_sub(1);
+        self.update_zero_and_negative_flags(self.register_y);
+    }
+
     fn branch(&mut self, condition: bool) {
         if condition {
             let jump = self.mem_read(self.program_counter);
