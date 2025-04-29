6525246d3771fba2a41a1e06b27e250f0bdd4bfd
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-04-29 21:30:39 +0900

    add iny

--- src/cpu.rs
+++ src/cpu.rs
@@ -372,6 +372,12 @@ impl CPU {
         self.update_zero_and_negative_flags(self.register_x);
     }
 
+    #[allow(dead_code)]
+    fn iny(&mut self) {
+        self.register_y.wrapping_add(1);
+        self.update_zero_and_negative_flags(self.register_y);
+    }
+
     fn branch(&mut self, condition: bool) {
         if condition {
             let jump = self.mem_read(self.program_counter);
