b956505e49434e11d444b7bdb2109adbdd0d37ac
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-05-06 15:43:24 +0900

    add tay

--- src/cpu.rs
+++ src/cpu.rs
@@ -627,8 +627,13 @@ impl CPU {
 
     fn tax(&mut self) {
         self.register_x = self.register_a;
-
         self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    #[allow(dead_code)]
+    fn tay(&mut self) {
+        self.register_y = self.register_a;
+        self.update_zero_and_negative_flags(self.register_y);
     }
 
     fn branch(&mut self, condition: bool) {
