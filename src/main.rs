fb8352374364917edea8d3e16aee3ba2e26a2a4f
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-05-15 21:35:57 +0900

    add main procs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
@@ -53,9 +54,21 @@ fn main() {
     let mut cpu = CPU::new();
     cpu.load(game_code);
     cpu.reset();
+
+    let mut screen_state = [0 as u8; 32 * 3 * 32];
+    let mut rng = rand::rng();
+
     cpu.run_with_callback(move |cpu| {
-        // TODO
-        print!("{}", cpu.register_a);
+        handle_user_input(cpu, &mut event_pump);
+        cpu.mem_write(0xfe, rng.random_range(1..16));
+
+        if read_screen_state(cpu, &mut screen_state) {
+            texture.update(None, &screen_state, 32 * 3).unwrap();
+            canvas.copy(&texture, None, None).unwrap();
+            canvas.present();
+        }
+
+        ::std::thread::sleep(std::time::Duration::new(0, 70_000));
     });
 }
 
