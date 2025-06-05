3c5685e9f04f761f74e3d394f1ac297d55082582
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-06-05 20:17:30 +0900

    add bus data access

--- src/bus.rs
+++ src/bus.rs
@@ -17,7 +17,32 @@ const PPU_REGISTERS_MIRROR_END: u16 = 0x3FFF;
 
 impl Memory for Bus {
     fn mem_read(&self, addr: u16) -> u8 {
-        match addr {}
+        match addr {
+            RAM..=RAM_MIRRORS_END => {
+                let mirror_down_addr = addr & 0b00000111_11111111;
+                self.cpu_vram[mirror_down_addr as usize]
+            }
+            PPU_REGISTERS..=PPU_REGISTERS_MIRROR_END => {
+                let mirror_down_addr = addr & 0b00100000_00000111;
+                todo!("PPU is not supported yet")
+            }
+            _ => {
+                println("Ignoring memory access at {}", addr);
+                0
+            }
+        }
     }
-    fn mem_write(&self, addr: u16, data: u8) {}
+    fn mem_write(&self, addr: u16, data: u8) {
+        match addr {
+            RAM..=RAM_MIRRORS_END => {
+                let mirror_down_addr = addr & 0b00000111_11111111;
+                self.cpu_vram[mirror_down_addr as usize] = data;
+            }
+            PPU_REGISTERS..=PPU_REGISTERS_MIRROR_END => {
+                let mirror_down_addr = addr & 0b00100000_00000111;
+                todo!("PPU is not supported yet")
+            }
+            _ => println("Ignoring memory access at {}", addr),
+        }
+    }
 }
