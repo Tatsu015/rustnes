b1457690ee7308f0155d20e35a22264aa433444b
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-06-07 17:02:45 +0900

    add check ines ver

--- src/rom.rs
+++ src/rom.rs
@@ -11,3 +11,17 @@ pub struct Rom {
     pub mapper: u8,
     pub screen_mirroring: Mirroing,
 }
+
+impl Rom {
+    pub fn new(raw: &Vec<u8>) -> Result<Rom, String> {
+        if &raw[0..4] != NES_TAG {
+            return Err("File is not in iNES file format".to_string());
+        }
+
+        let mapper = (raw[7] & 0b1111_0000) | (raw[6] >> 4);
+        let ines_ver = (raw[7] >> 2) & 0b11;
+        if ines_ver != 0 {
+            return Err("NES2.0 format is not supported".to_string());
+        }
+    }
+}
