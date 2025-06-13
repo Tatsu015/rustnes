dc531612087f61cc46d0873e3a74ab8b0f19a109
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-06-13 21:48:13 +0900

    fix test but not move

--- src/cpu.rs
+++ src/cpu.rs
@@ -790,48 +790,47 @@ mod test {
         let rom = Rom::new(&vec![]).unwrap();
         let bus = Bus::new(rom);
         let mut cpu = CPU::new(bus);
-        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
+        cpu.run();
         assert!(cpu.status.contains(CpuFlags::ZERO))
     }
 
     #[test]
     fn test_0xaa_tax_move_a_to_x() {
-        let rom = Rom::new(&vec![]).unwrap();
+        let rom = Rom::new(&vec![0xa9, 0x0a, 0xaa, 0x00]).unwrap();
         let bus = Bus::new(rom);
         let mut cpu = CPU::new(bus);
-        cpu.load_and_run(vec![0xa9, 0x0a, 0xaa, 0x00]);
+        cpu.run();
         assert_eq!(cpu.register_x, 10)
     }
 
     #[test]
     fn test_inx_overflow() {
-        let rom = Rom::new(&vec![]).unwrap();
+        let rom = Rom::new(&vec![0xa9, 0xff, 0xaa, 0xe8, 0xe8]).unwrap();
         let bus = Bus::new(rom);
         let mut cpu = CPU::new(bus);
         cpu.register_x = 0xff;
-        cpu.load_and_run(vec![0xa9, 0xff, 0xaa, 0xe8, 0xe8]);
+        cpu.run();
 
         assert_eq!(cpu.register_x, 1)
     }
 
     #[test]
     fn test_5_ops_working_togather() {
-        let rom = Rom::new(&vec![]).unwrap();
+        let rom = Rom::new(&vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]).unwrap();
         let bus = Bus::new(rom);
         let mut cpu = CPU::new(bus);
-        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+        cpu.run();
 
         assert_eq!(cpu.register_x, 0xc1)
     }
 
     #[test]
     fn test_lda_from_memory() {
-        let rom = Rom::new(&vec![]).unwrap();
+        let rom = Rom::new(&vec![0xa5, 0x10, 0x00]).unwrap();
         let bus = Bus::new(rom);
         let mut cpu = CPU::new(bus);
         cpu.mem_write(0x10, 0x55);
-
-        cpu.load_and_run(vec![0xa5, 0x10, 0x00]);
+        cpu.run();
 
         assert_eq!(cpu.register_a, 0x55)
     }
