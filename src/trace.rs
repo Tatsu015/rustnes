692df767fe71c26e5bed896146cf570405bc5e98
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-06-23 23:50:58 +0900

    add trace data

--- src/trace.rs
+++ src/trace.rs
@@ -1,14 +1,23 @@
-use crate::cpu::CPU;
+use std::collections::HashMap;
+
+use crate::cpu::{Memory, CPU};
+use crate::opcode;
 
 pub fn trace(cpu: &CPU) -> String {
+    let ref opcodes: HashMap<u8, &'static opcode::OpCode> = *opcode::OPECODE_MAP;
+
+    let code = cpu.mem_read(cpu.program_counter);
+    let ops = opcodes.get(&code).unwrap();
+
     println!(
-        "{:04X} A:{:02X} X:{:02X} Y:{:02X} P:{:02X} SP:{:02X}",
+        "{:04X} {:02X} A:{:02X} X:{:02X} Y:{:02X} P:{:02X} SP:{:02X}",
         cpu.program_counter,
+        ops.code,
         cpu.register_a,
         cpu.register_x,
         cpu.register_y,
         cpu.status,
         cpu.stack_pointer
     );
-    return "test".to_owned();
+    return "".to_owned();
 }
