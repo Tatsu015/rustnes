c9bc3f629615bf636a8245b11e2595ed11723672
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-06-24 22:52:39 +0900

    add operands

--- src/trace.rs
+++ src/trace.rs
@@ -9,10 +9,26 @@ pub fn trace(cpu: &CPU) -> String {
     let code = cpu.mem_read(cpu.program_counter);
     let ops = opcodes.get(&code).unwrap();
 
+    let low_operand = if ops.len > 1 {
+        format!("{:02X}", cpu.mem_read(cpu.program_counter + 1))
+    } else {
+        "  ".to_string()
+    };
+
+    let high_operand = if ops.len > 2 {
+        format!("{:02X}", cpu.mem_read(cpu.program_counter + 2))
+    } else {
+        "  ".to_string()
+    };
+
     println!(
-        "{:04X} {:02X} A:{:02X} X:{:02X} Y:{:02X} P:{:02X} SP:{:02X}",
+        "{:04X}  {:02X} {:} {:}  {:} {:}     A:{:02X} X:{:02X} Y:{:02X} P:{:02X} SP:{:02X}",
         cpu.program_counter,
         ops.code,
+        low_operand,
+        high_operand,
+        ops.mnemonic,
+        ops.len,
         cpu.register_a,
         cpu.register_x,
         cpu.register_y,
