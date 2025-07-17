7a641523ced2778c02ffaa03fb74e541117ece1b
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-07-17 23:25:38 +0900

    add 0x8f

--- src/opcode.rs
+++ src/opcode.rs
@@ -278,7 +278,7 @@ lazy_static! {
 
         OpCode::new(0x83, "*SAX", 2, 6, AddressingMode::Indirect_X),
         OpCode::new(0x87, "*SAX", 2, 3, AddressingMode::ZeroPage),
-        OpCode::new(0x8f, "*SAX", 2, 4, AddressingMode::Absolute),
+        OpCode::new(0x8f, "*SAX", 3, 4, AddressingMode::Absolute),
         OpCode::new(0x97, "*SAX", 2, 4, AddressingMode::ZeroPage_Y),
     ];
 
