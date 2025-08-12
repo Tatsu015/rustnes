093bb5d3f4c86fcac96d5269a268edadd9e7cea4
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-08-12 21:56:07 +0900

    add status register bit def

--- src/ppu.rs
+++ src/ppu.rs
@@ -24,6 +24,12 @@ bitflags! {
         const EMPHASIZE_GREEN = 0b0100_0000;
         const EMPHASIZE_BLUE = 0b1000_0000;
     }
+
+    pub struct StatusRegister:u8 {
+        const SPRITE_OVERFLOW_FLAG = 0b0010_0000;
+        const SPRITE_ZERO_HIT = 0b0100_0000;
+        const VBLANK_STARTED = 0b1000_0000;
+    }
 }
 
 pub struct ScrollRegister {
