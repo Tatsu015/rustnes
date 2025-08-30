c6687bdb44dfa5a2b0acf5bd337fb624f88e26b8
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-08-30 23:24:48 +0900

    impl frame

--- src/frame.rs
+++ src/frame.rs
@@ -5,4 +5,19 @@ pub struct Frame {
 impl Frame {
     const WUDTH: usize = 256;
     const HEIGHT: usize = 240;
+
+    pub fn new() -> Self {
+        Frame {
+            data: vec![0; (Frame::WUDTH) * (Frame::HEIGHT) * 3],
+        }
+    }
+
+    pub fn set_pixcel(&mut self, x: usize, y: usize, rgb: (u8, u8, u8)) {
+        let base = y * 3 * Frame::WUDTH + x * 3;
+        if base + 2 < self.data.len() {
+            self.data[base] = rgb.0;
+            self.data[base + 1] = rgb.1;
+            self.data[base + 2] = rgb.2;
+        }
+    }
 }
