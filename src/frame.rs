a9087162216a29779564f6b586a3e58907137b0b
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-09-01 23:35:45 +0900

    add show tile

--- src/frame.rs
+++ src/frame.rs
@@ -1,3 +1,5 @@
+use sdl2::pixels::Palette;
+
 #[rustfmt::skip]
 
 pub static SYSTEM_PALLETE: [(u8,u8,u8); 64] = [
@@ -38,4 +40,35 @@ impl Frame {
             self.data[base + 2] = rgb.2;
         }
     }
+}
+
+fn show_tile(chr_rom: &Vec<u8>, bank: usize, tile_n: usize) -> Frame {
+    assert!(bank <= 11);
+
+    let mut frame = Frame::new();
+    let bank = (bank * 0x1000) as usize;
+
+    let tile_start_pos = bank + tile_n * 16;
+    let tile = &chr_rom[tile_start_pos..=tile_start_pos + 15];
+
+    for y in 0..7 {
+        let mut upper = tile[y];
+        let mut lower = tile[y + 8];
+
+        for x in (0..=7).rev() {
+            let value = (1 & upper) << 1 | (1 & lower);
+            upper = upper >> 1;
+            lower = lower >> 1;
+            let rgb = match value {
+                0 => SYSTEM_PALLETE[0x01],
+                1 => SYSTEM_PALLETE[0x23],
+                2 => SYSTEM_PALLETE[0x27],
+                3 => SYSTEM_PALLETE[0x30],
+                _ => panic!("can't be"),
+            };
+            frame.set_pixcel(x, y, rgb)
+        }
+    }
+
+    frame
 }
