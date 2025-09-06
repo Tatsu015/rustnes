d4f97fa6c10f86d5acd2d601e102ec8e45daa793
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-09-06 20:37:09 +0900

    rename

---
+++ src/test_tile.rs
@@ -0,0 +1,165 @@
+use rand::Rng;
+use sdl2::event;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::EventPump;
+
+use crate::bus::Bus;
+use crate::cartoridge::Rom;
+use crate::cpu::Memory;
+use crate::frame::show_tile_brank;
+use cpu::CPU;
+
+pub mod bus;
+pub mod cartoridge;
+pub mod control;
+pub mod cpu;
+pub mod frame;
+pub mod mask;
+pub mod opcode;
+pub mod ppu;
+pub mod scroll;
+pub mod status;
+pub mod trace;
+
+fn main() {
+    const LOGICAL_WIDTH: u32 = 256;
+    const LOGICAL_HEIGHT: u32 = 240;
+    const WINDOW_SCALE: u32 = 3;
+    const BYTES_PER_PIXEL: u32 = 3;
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window(
+            "Snake game",
+            (LOGICAL_WIDTH * WINDOW_SCALE) as u32,
+            (LOGICAL_HEIGHT * WINDOW_SCALE) as u32,
+        )
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    canvas
+        .set_scale(WINDOW_SCALE as f32, WINDOW_SCALE as f32)
+        .unwrap();
+
+    let creator = canvas.texture_creator();
+    let mut texture = creator
+        .create_texture_target(PixelFormatEnum::RGB24, LOGICAL_WIDTH, LOGICAL_HEIGHT)
+        .unwrap();
+
+    let bytes = std::fs::read("./test/sample/sample1.nes").unwrap();
+    let rom = Rom::new(&bytes).unwrap();
+
+    let tile_frame = show_tile_brank(&rom.chr_rom, 1);
+    texture
+        .update(
+            None,
+            &tile_frame.data,
+            (LOGICAL_WIDTH * BYTES_PER_PIXEL) as usize,
+        )
+        .unwrap();
+    canvas.copy(&texture, None, None).unwrap();
+    canvas.present();
+
+    loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => std::process::exit(0),
+                _ => { /* nop */ }
+            }
+        }
+    }
+
+    // let bus = Bus::new(rom);
+    // let mut cpu = CPU::new(bus);
+    // cpu.reset();
+    // // TODO for test rom
+    // cpu.program_counter = 0xC000;
+
+    // let mut screen_state = [0 as u8; 32 * 3 * 32];
+    // let mut rng = rand::rng();
+
+    // cpu.run_with_callback(move |cpu| {
+    //     // println!("{}", trace(cpu))
+    //     handle_user_input(cpu, &mut event_pump);
+
+    //     cpu.mem_write(0xfe, rng.random_range(1..16));
+
+    //     if read_screen_state(cpu, &mut screen_state) {
+    //         texture.update(None, &screen_state, 32 * 3).unwrap();
+    //         canvas.copy(&texture, None, None).unwrap();
+    //         canvas.present();
+    //     }
+
+    //     std::thread::sleep(std::time::Duration::new(0, 70_000));
+    // });
+}
+
+// fn handle_user_input(cpu: &mut CPU, event_pump: &mut EventPump) {
+//     for e in event_pump.poll_iter() {
+//         match e {
+//             Event::Quit { .. }
+//             | Event::KeyDown {
+//                 keycode: Some(Keycode::Escape),
+//                 ..
+//             } => std::process::exit(0),
+//             Event::KeyDown {
+//                 keycode: Some(Keycode::W),
+//                 ..
+//             } => cpu.mem_write(0xff, 0x77),
+//             Event::KeyDown {
+//                 keycode: Some(Keycode::S),
+//                 ..
+//             } => cpu.mem_write(0xff, 0x73),
+//             Event::KeyDown {
+//                 keycode: Some(Keycode::A),
+//                 ..
+//             } => cpu.mem_write(0xff, 0x61),
+//             Event::KeyDown {
+//                 keycode: Some(Keycode::D),
+//                 ..
+//             } => cpu.mem_write(0xff, 0x64),
+//             _ => { /* nop */ }
+//         }
+//     }
+// }
+
+// fn color(byte: u8) -> Color {
+//     match byte {
+//         0 => sdl2::pixels::Color::BLACK,
+//         1 => sdl2::pixels::Color::WHITE,
+//         2 | 9 => sdl2::pixels::Color::GRAY,
+//         3 | 10 => sdl2::pixels::Color::RED,
+//         4 | 11 => sdl2::pixels::Color::GREEN,
+//         5 | 12 => sdl2::pixels::Color::BLUE,
+//         6 | 13 => sdl2::pixels::Color::MAGENTA,
+//         7 | 14 => sdl2::pixels::Color::YELLOW,
+//         _ => sdl2::pixels::Color::CYAN,
+//     }
+// }
+
+// fn read_screen_state(cpu: &mut CPU, frame: &mut [u8; 32 * 3 * 32]) -> bool {
+//     let mut frame_idx = 0;
+//     let mut update = false;
+//     for i in 0x0200..0x600 {
+//         let color_idx = cpu.mem_read(i as u16);
+//         let (b1, b2, b3) = color(color_idx).rgb();
+//         if frame[frame_idx] != b1 || frame[frame_idx + 1] != b2 || frame[frame_idx + 2] != b3 {
+//             frame[frame_idx] = b1;
+//             frame[frame_idx + 1] = b2;
+//             frame[frame_idx + 2] = b3;
+//             update = true;
+//         }
+//         frame_idx += 3;
+//     }
+//     update
+// }
