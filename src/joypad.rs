3f6a76ceea4282013601f6ccc4ed5502bc9b86c2
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-10-29 23:02:22 +0900

    add write and read

--- src/joypad.rs
+++ src/joypad.rs
@@ -27,4 +27,22 @@ impl Joypad {
             button_status: JoypadButton::from_bits_truncate(0),
         }
     }
+
+    pub fn write(&mut self, data: u8) {
+        self.strobe = data & 1 == 1;
+        if self.strobe {
+            self.button_index = 0;
+        }
+    }
+
+    pub fn read(&mut self) -> u8 {
+        if self.button_index > 7 {
+            return 1;
+        }
+        let response = (self.button_status.bits() & (1 << self.button_index)) >> self.button_index;
+        if !self.strobe && self.button_index <= 7 {
+            self.button_index += 1;
+        }
+        response
+    }
 }
